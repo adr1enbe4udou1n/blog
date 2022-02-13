ea52232bcc71d0fed1ac9bfeb838738d729092a7
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2022-02-13 14:36:00 +0100

    bump cargo theme v2

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/adr1enbe4udou1n/blog
 
 go 1.17
 
-require github.com/jpanther/congo v1.6.0 // indirect
+require (
+	github.com/jpanther/congo/v2 v2.0.4 // indirect
+)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  mode: "jit",
-  corePlugins: {
-    preflight: false
-  },
   content: [
     "./layouts/**/*.html",
     "./content/**/*.{html,md}",
@@ -10,47 +6,4 @@ module.exports = {
   safelist: [
     'sm:flex-row',
   ],
-  darkMode: "class",
-  theme: {
-    colors: {
-      transparent: "transparent",
-      neutral: {
-        DEFAULT: "var(--color-neutral)",
-        50: "var(--color-neutral-50)",
-        100: "var(--color-neutral-100)",
-        200: "var(--color-neutral-200)",
-        300: "var(--color-neutral-300)",
-        400: "var(--color-neutral-400)",
-        500: "var(--color-neutral-500)",
-        600: "var(--color-neutral-600)",
-        700: "var(--color-neutral-700)",
-        800: "var(--color-neutral-800)",
-        900: "var(--color-neutral-900)",
-      },
-      primary: {
-        50: "var(--color-primary-50)",
-        100: "var(--color-primary-100)",
-        200: "var(--color-primary-200)",
-        300: "var(--color-primary-300)",
-        400: "var(--color-primary-400)",
-        500: "var(--color-primary-500)",
-        600: "var(--color-primary-600)",
-        700: "var(--color-primary-700)",
-        800: "var(--color-primary-800)",
-        900: "var(--color-primary-900)",
-      },
-      secondary: {
-        50: "var(--color-secondary-50)",
-        100: "var(--color-secondary-100)",
-        200: "var(--color-secondary-200)",
-        300: "var(--color-secondary-300)",
-        400: "var(--color-secondary-400)",
-        500: "var(--color-secondary-500)",
-        600: "var(--color-secondary-600)",
-        700: "var(--color-secondary-700)",
-        800: "var(--color-secondary-800)",
-        900: "var(--color-secondary-900)",
-      },
-    },
-  },
 };
