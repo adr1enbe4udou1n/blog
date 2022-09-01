706047e4517e755d33abd015f8559dbfd9d5108a
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2022-09-02 00:19:00 +0200

    enhance theme integration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
-module.exports = {
-  darkMode: 'class',
-  content: [
-    "./layouts/**/*.html",
-    "./content/**/*.{html,md}",
-  ],
-  safelist: [
-    'sm:flex-row', 'lg:flex-row', 'lg:hidden', 'sm:block'
-  ],
+const colors = require("./themes/congo/node_modules/tailwindcss/colors");
+const config = require("./themes/congo/tailwind.config.js");
+
+config.theme.colors = {
+  ...config.theme.colors,
+  ...{
+    purple: config.theme.colors.primary,
+    black: colors.black,
+    white: colors.white,
+    gray: colors.gray,
+    emerald: colors.emerald,
+    red: colors.red,
+    indigo: colors.indigo,
+    yellow: colors.yellow,
+    teal: colors.teal,
+    orange: colors.orange,
+    green: colors.green,
+  },
 };
+
+module.exports = config;
